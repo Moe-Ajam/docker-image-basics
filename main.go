8f7746b5053d7f37c850d6cada7706111b30e72a
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-10-29 22:44:19 +0300

    made a sever and added a root path to serve an html page

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	port := ":8080"
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+	fmt.Printf("Listening on port: %s\n", port)
+
+	mux.HandleFunc("GET /", handlePage)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handlePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(200)
+	const page = `<html>
+<head></head>
+<body>
+	<p> Hello from Docker! I'm a Go server. </p>
+</body>
+</html>
+`
+	w.Write([]byte(page))
 }
